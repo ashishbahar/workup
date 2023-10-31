9a67419d5f2d12870fb7e298a1fc74304711165f
ashishbahar <ashishbahar987@gmail.com>
2023-10-31 17:01:08 +0530

    change

---
+++ src/components/Icon.jsx
@@ -0,0 +1,27 @@
+export fbIcon =()
+={    return(
+        <svg width="62" height="63" viewBox="0 0 62 63" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_231_1464)">
+<circle cx="31" cy="27.8145" r="17" fill="#FAFAFA"/>
+</g>
+<g clip-path="url(#clip0_231_1464)">
+<path d="M32.9987 23.807H34.0942V21.899C33.9052 21.873 33.2552 21.8145 32.4982 21.8145C30.9187 21.8145 29.8367 22.808 29.8367 24.634V26.3145H28.0938V28.4475H29.8367V33.8145H31.9737V28.448H33.6462L33.9117 26.315H31.9732V24.8455C31.9737 24.229 32.1397 23.807 32.9987 23.807Z" fill="#1A202C"/>
+</g>
+<defs>
+<filter id="filter0_d_231_1464" x="0" y="0.814453" width="62" height="62" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="7"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_231_1464"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_231_1464" result="shape"/>
+</filter>
+<clipPath id="clip0_231_1464">
+<rect width="12" height="12" fill="white" transform="translate(25 21.8145)"/>
+</clipPath>
+</defs>
+</svg>
+
+    )
+)}
