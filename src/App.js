307a407bef016efcd09d5f7538db48810e66c0c4
ujjwalkaushik111 <ujjwalsharma34567@gmail.com>
2023-11-01 11:09:31 +0530

    mission

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Hero from "./components/Hero";
+import OurMission from "./components/OurMission";
 
 function App() {
   return (
     <div>
       <Hero />
+      <OurMission/>
     </div>
   );
 }
---
+++ src/components/OurMission.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const OurMission = () => {
+  return (
+   <section>
+      <Container>
+        shgsghgfhgfhsg
+      </Container>
+   </section>
+  )
+}
+
+export default OurMission
