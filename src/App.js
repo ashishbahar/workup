749660ac3ac853929db85a0bbf44812c77e30c97
ashishbahar <ashishbahar987@gmail.com>
2023-11-01 12:25:47 +0530

    change

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Hero from "./components/Hero";
+import Feature from "./components/Feature";
 
 function App() {
   return (
     <div>
       <Hero />
+      <Feature />
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,27 +2,28 @@ import React from "react";
 import Header from "./Header";
 import { Container } from "react-bootstrap";
 import { ArrowIcon } from "./Icon";
-import mainimg from "../images/png/heroimg.png";
+import mainimg from "../assets/images/png/heroimg.png";
 const Hero = () => {
   return (
     <div className="hero_linear">
       <Header />
-      <Container className="custom_container py-5">
+      <Container className="custom_container py-4 sm:py-5">
         <h1 className=" text-center ff_outfit text_black leading_120 fw-semibold fs_xl">
           Superpower your business finance
         </h1>
         <p className=" ff_outfit fw-normal pb-2 fs_xlg text-center text-linear mt-3 pt-1">
           Empowering Autopilot Automation
         </p>
-        <div className="hero_input  mt-5  mx-auto py-2 ps-4 pe-2">
+        <div className="hero_input position-relative mt-4   sm:mt-5  mx-auto py-2 ps-3 sm:ps-4 pe-2">
+          <div className="hero_shadow z-n1 position-absolute "></div>
           <form action="">
             <input
               required
               type="email"
-              className="border-0 w-85 "
+              className="border-0 w-85 w_75 w-85"
               placeholder="Enter email to join waitlist"
             />
-            <button className="hero_btn border-0">
+            <button className="hero_btn w_25  border-0">
               <ArrowIcon />
             </button>
           </form>
