dee11ff357f6ae8f7ec0e564f93ebb71b30fb82f
ujjwalkaushik111 <ujjwalsharma34567@gmail.com>
2023-11-02 15:24:49 +0530

    code

--- src/components/Working.jsx
+++ src/components/Working.jsx
@@ -1,26 +1,42 @@
 import React from "react";
-import { Bluesvg } from "./Iconsvg";
+import { Arrow, BlueWorking, Search, Security } from "./Iconsvg";
 import { Col, Container, Row } from "react-bootstrap";
-import Workings from "../assets/images/png2/Working.png"
+import Workings from "../assets/images/png2/Working.png";
+import { WorkingData } from "./Helper";
 
 const Working = () => {
   return (
     <section className="Bg-sky">
       <Container className="custom_container py-5">
         <div className="d-flex justify-content-center mb-2">
-          <Bluesvg />
+          <BlueWorking />
         </div>
         <h2 className="ff_outfit fw-medium fs_lg text-lightblack text-center">
           How it’s working
         </h2>
         <Row className="align-items-center mt-4">
           <Col lg={6}>
-            <img className="w-75" src={Workings} alt="workings" />
+            <img className="w-100" src={Workings} alt="workings" />
           </Col>
           <Col lg={6}>
-            <div>
-              
-            </div>
+            {WorkingData &&
+              WorkingData.map((obj, i) => {
+                return (
+                  <div className="BoxWorking mb-3">
+                    <div className="d-flex gap-4">
+                      <div>{obj.SvgIcon}</div>
+                      <div>
+                        <h2 className="ff_outfit fs_md fw-medium text_lightblack line-height-Sign">
+                          {obj.Heading}
+                        </h2>
+                        <p className="ff_Inter fs_sm fw-normal text-black Para_working_width">
+                          {obj.Para}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
           </Col>
         </Row>
       </Container>
